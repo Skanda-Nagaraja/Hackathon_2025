d22662133fac45b5726eb281b7a54425dae80a5a
Skanda-Nagaraja <s632003130@tamu.edu>
2025-01-26 03:20:21 -0600

    added points to context doc

--- hackathon-25/src/app/UserContext.tsx
+++ hackathon-25/src/app/UserContext.tsx
@@ -5,16 +5,15 @@ import { useRouter } from "next/navigation";
 
 // Define UserContext
 interface UserContextType {
-  user: { id: string; username: string } | null;
-  setUser: (user: { id: string; username: string } | null) => void;
+  user: { id: string; username: string; points: number } | null;
+  setUser: (user: { id: string; username: string; points: number } | null) => void;
   logout: () => void;
 }
 
 const UserContext = createContext<UserContextType | undefined>(undefined);
 
-// Create Provider
 export function UserProvider({ children }: { children: React.ReactNode }) {
-  const [user, setUser] = useState<{ id: string; username: string } | null>(null);
+  const [user, setUser] = useState<{ id: string; username: string; points: number } | null>(null);
   const router = useRouter();
 
   const logout = () => {
@@ -29,7 +28,6 @@ export function UserProvider({ children }: { children: React.ReactNode }) {
   );
 }
 
-// Custom hook to use UserContext
 export function useUser() {
   const context = useContext(UserContext);
   if (!context) {
